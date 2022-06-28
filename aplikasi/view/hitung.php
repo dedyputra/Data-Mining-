f75a1adb99b7530b06962482baf4e2ce34025c9d
Dedy Putra Aditia <73837147+dedyputra@users.noreply.github.com>
2022-06-28 09:32:33 +0700

    TA Data Mining

---
+++ aplikasi/view/hitung.php
@@ -0,0 +1,106 @@
+ <?php
+  include "./template/dashboard.php";
+  include "../control/functions.php";
+  ?>
+ <!DOCTYPE html>
+ <html lang="en">
+
+ <head>
+   <meta charset="utf-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+   <meta name="description" content="">
+   <meta name="author" content="">
+
+   <title>Data Review</title>
+   <!-- Custom fonts for this template -->
+   <link href="../../asset/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+   <link href="../../asset/https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+   <!-- Custom styles for this template -->
+   <link href="../../asset/css/sb-admin-2.min.css" rel="stylesheet">
+   <!-- Custom styles for this page -->
+   <link href="../../asset/vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
+ </head>
+
+ <body class="bg-gradient-primary">
+   <div class="container mb-10">
+     <div class="row justify-content-center">
+       <div class="col-12">
+         <div class="card">
+           <div class="card-header py-3">
+             <h6 class="m-0 font-weight-bold text-primary">Proses hitung</h6>
+           </div>
+           <div class="card-body">
+             <!-- <form action="data_survey.php" method="POST" class="user" aria-describedby="emailHelp">
+               <div class="mb-3">
+                 <label for="ID" class="form-label">ID</label>
+                 <input type="text" class="form-control" id="ID" aria-describedby="emailHelp">
+               </div>
+               <div class="mb-3">
+                 <label for="X1" class="form-label">X1</label>
+                 <input type="number" class="form-control" id="X1">
+               </div>
+               <div class="mb-3">
+                 <label for="X2" class="form-label">X2</label>
+                 <input type="number" class="form-control" id="X2">
+               </div>
+               <div class="mb-3">
+                 <label for="X3" class="form-label">X3</label>
+                 <input type="number" class="form-control" id="X1">
+               </div>
+               <div class="mb-3">
+                 <label for="X4" class="form-label">X4</label>
+                 <input type="number" class="form-control" id="X4">
+               </div>
+               <div class="mb-3">
+                 <label for="X5" class="form-label">X5</label>
+                 <input type="number" class="form-control" id="X5">
+               </div>
+               <div class="mb-3">
+                 <label for="X6" class="form-label">X6</label>
+                 <input type="number" class="form-control" id="X6">
+               </div>
+               <div class="mb-3">
+                 <label for="X7" class="form-label">X7</label>
+                 <input type="number" class="form-control" id="X7">
+               </div>
+               <div class="mb-3">
+                 <label for="X8" class="form-label">X8</label>
+                 <input type="number" class="form-control" id="X8">
+               </div>
+               <div class="mb-3">
+                 <label for="X9" class="form-label">X9</label>
+                 <input type="number" class="form-control" id="X9">
+               </div>
+               <div class="mb-3">
+                 <label for="X10" class="form-label">X10</label>
+                 <input type="number" class="form-control" id="X10">
+               </div>
+               <div class="mb-3">
+                 <label for="X11" class="form-label">X11</label>
+                 <input type="number" class="form-control" id="X11">
+               </div>
+               <div class="mb-3">
+                 <label for="X12" class="form-label">X12</label>
+                 <input type="number" class="form-control" id="X12">
+               </div>
+               <button type="submit" name="submit" class="btn btn-primary">Tambah Data+</button>
+             </form> -->
+           </div>
+         </div>
+       </div>
+     </div>
+   </div>
+   <!-- Bootstrap core JavaScript-->
+   <script src="../../asset/vendor/jquery/jquery.min.js"></script>
+   <script src="../../asset/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+   <!-- Core plugin JavaScript-->
+   <script src="../../asset/vendor/jquery-easing/jquery.easing.min.js"></script>
+
+   <!-- Custom scripts for all pages-->
+   <script src="../../asset/js/sb-admin-2.min.js"></script>
+
+ </body>
+
+ </html>
